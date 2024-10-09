0e25b11c4dd8074be46c5d9ad500a53c076361db
meegan29 <1009700@mymail.sutd.edu.sg>
2024-10-09 19:42:04 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <title>Hello World</title>
-  <style></style>
 </head>
+
 <body>
-  <h1>Hello World!</h1>
-  <script></script>
+    <h1>Hello, World!</h1>
 </body>
+
 </html>
